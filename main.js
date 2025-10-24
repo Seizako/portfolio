40c1ecd58d345eddc5d910a9b0b8b4477ad5b7bc
Ludovic Weng <ludovic.weng@epitech.eu>
2025-10-24 17:34:13 +0200

    Modified

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let menu = document.getElementById("menu");
 let button = document.getElementById("menu_button");
 
 button.addEventListener("click", function() {
-    
+    menu.classList.toggle("active");
 })
